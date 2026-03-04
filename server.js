f0b32150a8d7d0f7d7902dfa1d0821f45fc91322
Sharppo <141234675+Sharppo@users.noreply.github.com>
2026-03-04 16:22:25 +0700

    Update server.js

--- server.js
+++ server.js
@@ -45,5 +45,8 @@ app.get('/api/analyze', async (req, res) => {
     }
 });
 
+// Di file server.js paling bawah
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+});
